0475d3d0b47d848854e0b71c622a75588f4e33d2
Sulian Thual <sulianthual@gmail.com>
2024-11-23 07:10:04 -0500

    20241123-071004

--- addons/scribbler/scribbler_menu.gd
+++ addons/scribbler/scribbler_menu.gd
@@ -13,6 +13,8 @@ extends Control
 @export var brush_color: Color=Color.BLACK
 ## Dialogue Control scene for resizing
 @export var resize_dialogue: PackedScene
+## Dialogue Control scene for sheet 
+@export var sheet_dialogue: PackedScene
 ## Draw mode (must match drawing.gd)
 @export_enum("regular","over","behind") var draw_mode: String="regular"
 var detached: bool=false# dock starts detached or not
@@ -41,7 +43,7 @@ var detached: bool=false# dock starts detached or not
 @onready var draw_mode_button: Button=%draw_mode
 @onready var brush_color_button: Button=%brush_color
 ## test
-#@onready var test: Button=%test
+@onready var test: Button=%test
 
 func _ready():
 	## drawer
@@ -62,7 +64,7 @@ func _ready():
 	draw_mode_button.connect("pressed",_on_draw_mode_pressed)
 	undo.connect("pressed",_on_undo_pressed)
 	hide_button.connect("pressed",_on_hide_pressed)
-	#test.connect("pressed",_on_test_pressed)
+	test.connect("pressed",_on_test_pressed)
 	## others
 	_update_mode()
 	_update_draw_mode()
@@ -366,3 +368,84 @@ func _help_dialogue():
 	file_dialogue.popup()
 	return file_dialogue
 	
+## TESsT
+func _on_test_pressed():
+	#_load_from_sheet_select_file_dialogue()# workds
+	_save_from_sheet_select_file_dialogue()
+
+## LOAD SCRIBBLE FROM SHEET
+var sheet_dialogue_input_subset: Array[int]=[1,1,1,1]# subx,suby,ix,iy, subset of source image
+var load_from_sheet_selected_file: String# pass selected file
+func _load_from_sheet_select_file_dialogue():
+	var file_dialogue = EditorFileDialog.new()
+	file_dialogue.clear_filters()
+	file_dialogue.file_mode = EditorFileDialog.FILE_MODE_OPEN_FILE
+	file_dialogue.access = EditorFileDialog.ACCESS_RESOURCES
+	file_dialogue.filters = ["*.png ; PNG File"]
+	file_dialogue.set_size(Vector2(640, 360))
+	file_dialogue.set_display_mode(EditorFileDialog.DisplayMode.DISPLAY_LIST)
+	EditorInterface.popup_dialog_centered(file_dialogue)
+	file_dialogue.connect("file_selected", _on_load_from_sheet_select_file_file_selected)
+	file_dialogue.popup()
+	return file_dialogue
+func _on_load_from_sheet_select_file_file_selected(input_file: String):
+	load_from_sheet_selected_file=input_file# keep for later dialogue
+	_load_from_sheet_select_subset_dialogue(input_file)
+func _load_from_sheet_select_subset_dialogue(input_file: String):
+	var file_dialogue = ConfirmationDialog.new()
+	file_dialogue.set_size(Vector2(640, 360))
+	file_dialogue.title="Select Sheet Subset"
+	EditorInterface.popup_dialog_centered(file_dialogue)
+	file_dialogue.connect("confirmed",_on_load_from_sheet_dialogue_confirmed)
+	var _dialogue: Control=sheet_dialogue.instantiate()
+	_dialogue.connect("subset_changed",_on_load_from_sheet_dialogue_subset_changed)
+	file_dialogue.add_child(_dialogue)
+	_dialogue.set_subset(sheet_dialogue_input_subset)
+	_dialogue.make_source_image(input_file)
+	file_dialogue.popup()
+	return file_dialogue
+func _on_load_from_sheet_dialogue_subset_changed(input_subset: Array[int]):# subx,suby,ix,iy
+	sheet_dialogue_input_subset=input_subset
+func _on_load_from_sheet_dialogue_confirmed():
+	if load_from_sheet_selected_file:
+		drawing.load_drawing_subset(load_from_sheet_selected_file, sheet_dialogue_input_subset)
+		load_from_sheet_selected_file=""
+
+## SAVE SCRIBBLE TO SHEET
+var save_from_sheet_selected_file: String# pass selected file
+func _save_from_sheet_select_file_dialogue():
+	var file_dialogue = EditorFileDialog.new()
+	file_dialogue.clear_filters()
+	file_dialogue.file_mode = EditorFileDialog.FILE_MODE_SAVE_FILE
+	file_dialogue.access = EditorFileDialog.ACCESS_RESOURCES
+	file_dialogue.filters = ["*.png ; PNG File"]
+	file_dialogue.set_size(Vector2(640, 360))
+	file_dialogue.set_display_mode(EditorFileDialog.DisplayMode.DISPLAY_LIST)
+	EditorInterface.popup_dialog_centered(file_dialogue)
+	file_dialogue.connect("file_selected", _on_save_from_sheet_select_file_file_selected)
+	file_dialogue.popup()
+	return file_dialogue
+func _on_save_from_sheet_select_file_file_selected(input_file: String):
+	save_from_sheet_selected_file=input_file# keep for later dialogue
+	_save_from_sheet_select_subset_dialogue(input_file)
+func _save_from_sheet_select_subset_dialogue(input_file: String):
+	var file_dialogue = ConfirmationDialog.new()
+	file_dialogue.set_size(Vector2(640, 360))
+	file_dialogue.title="Select Sheet Subset"
+	EditorInterface.popup_dialog_centered(file_dialogue)
+	file_dialogue.connect("confirmed",_on_load_from_sheet_dialogue_confirmed)
+	var _dialogue: Control=sheet_dialogue.instantiate()
+	_dialogue.connect("subset_changed",_on_load_from_sheet_dialogue_subset_changed)
+	file_dialogue.add_child(_dialogue)
+	_dialogue.set_subset(sheet_dialogue_input_subset)
+	_dialogue.make_source_image(input_file)
+	file_dialogue.popup()
+	return file_dialogue
+func _on_save_from_sheet_dialogue_subset_changed(input_subset: Array[int]):# subx,suby,ix,iy
+	sheet_dialogue_input_subset=input_subset
+func _on_save_from_sheet_dialogue_confirmed():
+	if save_from_sheet_selected_file:
+		## here load the file, blit and overwrite it...
+		#drawing.load_drawing_subset(load_from_sheet_selected_file, sheet_dialogue_input_subset)
+		save_from_sheet_selected_file=""
+		
---
+++ addons/scribbler/scribbler_sheet_dialog.gd
@@ -0,0 +1,79 @@
+@tool
+extends Control
+
+## Select subset from a source image
+
+@onready var source_image: TextureRect=%source_image
+@onready var grid_image: TextureRect=%grid_image
+@onready var subx: SpinBox=%subx
+@onready var suby: SpinBox=%suby
+@onready var ix: SpinBox=%ix
+@onready var iy: SpinBox=%iy
+
+signal subset_changed(value: Array[int])# array subx,suby,ix,iy
+
+func _ready():
+	for i in [subx,suby,ix,iy]:
+		i.connect("value_changed",on_subset_changed)
+
+
+################################################
+## CALLS
+
+func set_subset(input_subset: Array[int]):# subx,suby,ix,iy
+	subx.value=input_subset[0]
+	suby.value=input_subset[1]
+	ix.value=input_subset[2]
+	iy.value=input_subset[3]
+	
+func make_source_image(filename_: String):
+	if FileAccess.file_exists(filename_):
+		var img: Image=Image.new()
+		img.load(filename_)
+		img.convert(Image.FORMAT_RGBA8)
+		var _texture: ImageTexture=ImageTexture.create_from_image(img)
+		source_image.texture=_texture
+		update_grid()
+
+func on_subset_changed(input_value: float):# from any of subx,suby,ix,iy
+	if ix.value>subx.value:
+		ix.value=subx.value
+	elif iy.value>suby.value:
+		iy.value=suby.value
+	else:
+		var _array: Array[int]=[subx.value,suby.value,ix.value,iy.value]
+		subset_changed.emit(_array)
+		update_grid()# if/elif above will retrigger subset_changed and this
+	
+var grid_color: Color=Color.BLACK
+var grid_select_color: Color=Color.RED
+func update_grid():
+	if source_image and source_image.texture:
+		var img: Image=source_image.texture.get_image()
+		img.fill(Color.TRANSPARENT)
+		var subset_rect_w: int=roundi(img.get_width()/subx.value)
+		var subset_rect_h: int=roundi(img.get_height()/suby.value)
+		var subset_rect_x: int=subset_rect_w*clamp(ix.value-1,0,subx.value-1)
+		var subset_rect_y: int=subset_rect_h*clamp(iy.value-1,0,suby.value-1)
+		# columns
+		for j in img.get_height():
+			for ii in range(subx.value):	
+				var i: int=subset_rect_w*ii
+				img.set_pixel(i,j, grid_color)
+			img.set_pixel(img.get_width()-1,j, grid_color)# last
+		# rows
+		for i in img.get_width():
+			for jj in range(suby.value):	
+				var j: int=subset_rect_h*jj
+				img.set_pixel(i,j, grid_color)
+			img.set_pixel(i,img.get_height()-1, grid_color)# last
+		# selector (change color of grid)
+		for i in range(subset_rect_x,subset_rect_x+subset_rect_w-1):
+			for j in [subset_rect_y,subset_rect_y+subset_rect_h-1]:
+				img.set_pixel(i,j, grid_select_color)
+		for j in range(subset_rect_y,subset_rect_y+subset_rect_h-1):
+			for i in [subset_rect_x,subset_rect_x+subset_rect_w-1]:
+				img.set_pixel(i,j, grid_select_color)
+		# texture
+		var _texture: ImageTexture=ImageTexture.create_from_image(img)
+		grid_image.texture=_texture
