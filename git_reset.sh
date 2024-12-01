f7e6c4092b1027d1ab06bbc45cbf961c4598394b
Sulian Thual <sulianthual@gmail.com>
2024-12-01 09:28:35 -0500

    20241201-092835

--- git_reset.sh
+++ git_reset.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-#git reset
-#git checkout .
-#git clean -fdx
+echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+echo "GIT RESET:" 
+echo "this script will reset uncommited changes"
+echo "(git reset, git checkout ., get clean -dfx)"
+read -p "Confirm reset by entering yesido:" yesconfirm
+if [ "$yesconfirm" != "yesido" ]; then
+    echo "Did not confirm, Aborting"
+    exit 1
+fi
+
+git reset
+git checkout .
+git clean -fdx
